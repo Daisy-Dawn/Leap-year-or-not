aba3110f07b29707c2d3f3895125e352bdb96d70
Agbo Amarachukwu Emmanuella <agboella20@gmail.com>
2023-03-18 16:49:49 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Write a program that checks whether a year is a leap year or not
+
+int main(void) 
+{
+  int year;
+
+  printf("Please enter the year:\t");
+  scanf("%d", &year);
+    system("clear");
+
+  if (year % 4 == 0 && year % 100 != 0)
+    printf("The year %d is a leap year", year);
+
+  else
+    printf("The year %d is not a leap year\n", year);
+  
+printf("\nEnd of program\n\n");
+  sleep (5);
+  system("clear");
+  printf("Welcome user,");
+  
+  return 0;
+}
